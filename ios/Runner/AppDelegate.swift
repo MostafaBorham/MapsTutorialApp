28da558a1e527bdbbe301788ce4d85d197b94e35
MostafaBorham <mostafaborham7@gmail.com>
2025-02-25 20:28:41 +0200

    config google maps in project

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-  GMSServices.provideAPIKey("AIzaSyAZtezvuXfmfEFdBgDEOxQTZyuMtJoN2MA")
+  GMSServices.provideAPIKey("AIzaSyDwaZ4ldTj0iLNyJ6CJwtRTxfl0B1o-jyc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
